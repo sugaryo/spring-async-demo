b5d91140ee33d8b270aa1fda70d1e3ceb5302070
sugaryo <ryo.sugawara.1224@gmail.com>
2021-06-28 15:51:09 +0900

    [ADD] 検証用コードを追加。

--- spring-async-demo/src/main/java/sugaryo/demo/async/app/TaskController.java
+++ spring-async-demo/src/main/java/sugaryo/demo/async/app/TaskController.java
@@ -1,6 +1,8 @@
 package sugaryo.demo.async.app;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,12 +34,108 @@ public class TaskController {
 		
 		return token;
 	}
+
+	@RequestMapping("proc/err")
+	public String proc_e() {
+
+		log.info( "begin sync process() at {}", LocalDateTime.now() );
+
+		// 10秒 処理をやらせる。
+		log.info( "main thread task ▼call" );
+		var res = this.task.err( 10 );
+		log.info( "main thread task ▲called" );
+
+		
+		// 完了してないけどとりあえず。
+		return "done";
+	}
+	
+	@RequestMapping("proc/sync")
+	public String proc_c() {
+
+		log.info( "begin sync process() at {}", LocalDateTime.now() );
+
+		// 10秒 処理をやらせる。
+		log.info( "main thread task ▼call" );
+		var res = this.task.sync( 10 );
+		log.info( "main thread task ▲called" );
+
+		
+		// 完了してないけどとりあえず。
+		return "done";
+	}
+	
+	@RequestMapping("proc/async")
+	public String proc_a() {
+		
+		log.info( "begin async process() at {}", LocalDateTime.now() );
+		
+		// 10秒 処理をやらせる。
+		log.info( "main thread task ▼call" );
+		var await = this.task.process( 10 );
+		log.info( "main thread task ▲called" );
+		
+		// 5秒 待ってから止める
+		log.info( "main thread sleep(5)" );
+		sleep( 5 );
+		log.info( "main thread task stop." );
+		await.cancel( true );
+		log.info( "main thread task stoped (done={}).", await.isDone() );
+		
+		
+		// 完了してないけどとりあえず。
+		return "done";
+	}
+	@RequestMapping("proc/async2")
+	public String proc_b() {
+		
+		log.info( "begin async process() at {}", LocalDateTime.now() );
+		
+		// 10秒 処理をやらせる。
+		log.info( "main thread task ▼call" );
+		var await = this.task.process( 10 );
+		log.info( "main thread task ▲called" );
+		
+		// 5秒 待ってから止める
+		log.info( "main thread sleep(5)" );
+		sleep( 5 );
+		log.info( "main thread task stop." );
+		await.cancel( false );
+		log.info( "main thread task stoped (done={}).", await.isDone() );
+		
+		
+		// 完了してないけどとりあえず。
+		return "done";
+	}
+
+	static void sleep(long seconds) {
+		try {
+			TimeUnit.SECONDS.sleep( seconds );
+		}
+		catch (InterruptedException ex) {
+			throw new RuntimeException( ex );
+		}
+	}
 	
 	
     @ExceptionHandler(TaskRejectedException.class)
     @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     public String onTaskRejected() {
-    	
+    	log.warn( "task thread-pool capacity over" );
         return "too busy";
+    }
+    
+    @ExceptionHandler(InterruptedException.class) 
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public String interrupted() {
+    	log.warn( "thread interrupted-exception happened." );
+    	return "thread-interrupted...";
+    }
+    
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public String error() {
+    	log.warn( "unexpected-exception happened." );
+    	return "unexpected-error!!";
     }
 }
--- spring-async-demo/src/main/java/sugaryo/demo/async/app/TaskModule.java
+++ spring-async-demo/src/main/java/sugaryo/demo/async/app/TaskModule.java
@@ -1,10 +1,15 @@
 package sugaryo.demo.async.app;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
 
@@ -18,6 +23,34 @@ public class TaskModule {
 	
 	private static final Logger log = LoggerFactory.getLogger( TaskModule.class );
 	
+	public String err(final int n ) {
+		
+		for ( int i = 0; i < n; i++ ) {
+			
+			long x = i + 1;
+			log.info( "count {}", x );
+			sleep( 1 );
+		}
+		throw new RuntimeException( "予期せぬエラーが発生した気持ち" );
+	}
+	
+	public String sync(final int n) {
+
+		log.info( "start" );
+		sleep( n );
+		log.info( "end" );
+		return "done subthread task.";
+	}
+	
+	@Async
+	public Future<String> process(final int n) {
+
+		log.info( "start" );
+		sleep( n );
+		log.info( "end" );
+		
+		return new AsyncResult<String>( "done subthread task." );
+	}
 	
 	@Async
 	public void asyncHeavyJob(String token) {
@@ -32,7 +65,9 @@ public class TaskModule {
 			TimeUnit.SECONDS.sleep( seconds );
 		}
 		catch (InterruptedException ex) {
-			throw new RuntimeException( ex );
+			log.error( "★★ {}!!", ex.getMessage() );
+			//Thread.currentThread().interrupt();
+			throw new RuntimeException( ex ); // この例外キャッチされない。
 		}
 	}
 }
